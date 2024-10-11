b75e98d327d71815885183e45070036ec508a25a
Nannie Ingersdotter <emelie.l.gustafsson@gmail.com>
2024-10-11 20:31:32 +0200

    created tables

--- model/init_db.sql
+++ model/init_db.sql
@@ -10,9 +10,21 @@ SET foreign_key_checks = 1;
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
-    );
+CREATE TABLE `User`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `UserName` VARCHAR(255) NOT NULL,
+    `Password` VARCHAR(255) NOT NULL,
+    `Email` VARCHAR(255) NOT NULL,
+    `FirstName` VARCHAR(255) NOT NULL,
+    `LastName` VARCHAR(255) NOT NULL, 
+    `Preference` VARCHAR (255) NULL
+);
+CREATE TABLE `Fridge`(
+    `id` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Ingredient` VARCHAR(255) NOT NULL,
+    `ExpirationDate` DATE NULL,
+    `Category` VARCHAR(255) NOT NULL,
+    `Quantity` INT NOT NULL,
+    `Unit` VARCHAR(255) NOT NULL
+);
+
