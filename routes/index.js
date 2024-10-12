eb3603c25325018ee7b7e6746ef3becaf08c8116
Claire Carter <carter.claire.e@gmail.com>
2024-10-12 09:37:58 -0700

    Outlined endpoints needed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
+const mysql = require("mysql");
+// For protecting endpoints...modify depending on what Emelie calls guard
+var userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
-// Here is my new branch
+// Home/login view: likely all endpoints will come from users.js
+// Profile view: likely all endpoints will come from users.js 
+
+// Fridge view
+  // POST ingredients
+  // GET ingredients (join USERS for user's first name)
+  // DELETE ingredients
+  // POST ingredients to edit quantity
+
+// Recipe gallery view
+  // GET recipe cards from API (limit up to 20?), match based on fridge contents 
+
+
 
 module.exports = router;
