4a6230e438989f93a9237162e2f7b6906d406683
Nannie Ingersdotter <emelie.l.gustafsson@gmail.com>
2024-10-13 16:11:36 +0200

    User Endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,91 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require("jsonwebtoken");
 const db = require("../model/helper");
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
+
+
+const supersecret = process.env.SUPER_SECRET;
+
 
 // New branch
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+//helper
+async function getAllUsers(){
+  const result = await db ("SELECT * FROM User ORDER BY id ASC;");
+  return result.data;
+}
+
+// get all users
+
+router.get("/", async (req, res) => {
+  try {
+    console.log("Received request to fetch users");
+    const people = await getAllUsers();
+    res.send(people);
+  } catch (error) {
+    console.error("Error fetching users:", error); // Log errors to the console
+    res.status(500).send({ error: error.message });
+  }
 });
+
+
+//post users
+// login for the first time 
+
+router.post("/", async (req, res) => {
+  const { UserName, Password, Email, FirstName, LastName, Preference } = req.body;
+
+  try {
+    const hash = await bcrypt.hash(Password, saltRounds);
+
+    await db(
+      `INSERT INTO User (UserName, Password, Email, FirstName, LastName, Preference)
+       VALUES ("${UserName}", "${hash}", "${Email}","${FirstName}","${LastName}","${Preference}")`
+    );
+
+    res.send({ message: "Register successful" });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+//returning user
+
+router.post("/login", async (req, res) => {
+  const { UserName, Password } = req.body;
+
+  try {
+    const results = await db(
+      `SELECT * FROM users WHERE UserName = "${UserName}"`
+    );
+    const user = results.data[0];
+    if (user) {
+      const user_id = user.id;
+
+      const correctPassword = await bcrypt.compare(Password, user.Password);
+
+      if (!correctPassword) throw new Error("Incorrect password");
+
+      var token = jwt.sign({ user_id }, supersecret);
+      res.send({ message: "Login successful, here is your token", token });
+    } else {
+      throw new Error("User does not exist");
+    }
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+router.get("/profile", userShouldBeLoggedIn, (req, res) => {
+  res.send({
+    message: "Here is the PROTECTED data for user " + req.user_id,
+  });
+});
+
 
 
 
