3eae7a958aa804e0147c9a828492e61552cced65
Claire Carter <carter.claire.e@gmail.com>
2024-10-14 10:21:02 -0700

    Merged with main and set up database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors'); 
 
-var indexRouter = require('./routes/index');
+var fridgeRouter = require('./routes/fridge');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/index', indexRouter);
+app.use('/api/fridge', fridgeRouter);
 app.use('/api/users', usersRouter);
 
 module.exports = app;
