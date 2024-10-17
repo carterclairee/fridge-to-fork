d03bea2ec699bcef960eb5c063eca51cbe4c1c27
Claire Carter <carter.claire.e@gmail.com>
2024-10-16 22:02:16 -0700

    Recipes and cards get requests completed; have not yet included preferences

--- routes/fridge.js
+++ routes/fridge.js
@@ -95,11 +95,10 @@ router.put("/:id", userShouldBeLoggedIn, async (req, res) => {
 
 // Recipe view
 
-// This may need to be 2 GET requests nested
-
 // GET recipe cards from API (limit up to 10?), match based on fridge contents 
 
 // It looks like adding plus will trigger the ignorePantry. Maybe have a set number of those (like water, sugar, salt, pepper, oil?) each time?
+// I have not done anything with preference yet
 
 router.get("/recipes", userShouldBeLoggedIn, async (req, res) => {
   const user_id = req.user_id;
@@ -122,54 +121,49 @@ router.get("/recipes", userShouldBeLoggedIn, async (req, res) => {
     // If we want the user to be able to switch up the ranking: const ranking = req.query.ranking || 2;
     // The url would look like this: /recipes?ranking=2
 
+    // Number of recipes limit (I set low to reduce api calls right now but we can change)
+    const number = 2
+
     // Get recipe matches based on ingredients from Spoonacular
     const recipeMatchesCall = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients`, {
       params: {
         ingredients: fridgeList,
         apiKey: apiKey,
-        ranking: ranking
+        ranking: ranking,
+        number: number
       }
     });
     
     const recipeMatches = recipeMatchesCall.data;
-    
-    res.status(200).send(recipeMatches);
 
     // Get recipe matches cards from Spoonacular
+    // Need to map through the recipes array to get the id number. Will need an if statement in case of no matches
 
+    if (recipeMatches.length) {
+      // Getting promise first because will want to wait for all promises to be resolved before showing cards (will use Promise.all later)
+      const recipeCardsPromise = recipeMatches.map(async (recipe) => {
+        const recipeCardsCall = await axios.get(`https://api.spoonacular.com/recipes/${recipe.id}/card`, {
+          params: {
+            apiKey: apiKey
+          }
+        });
+        return {
+          // Want to send full recipe in case info is needed later (like missing ingredients)
+          ...recipe,
+          // Card is sent as a url, but I'm not sure how to display it later
+          card: recipeCardsCall.data.url
+        };
+      });
+
+      // Use Promise.all, which waits for all of the card requests to complete and is useful since we're doing 2 GETs at the same time here
+      const recipesAndCards = await Promise.all(recipeCardsPromise);
+
+      // Send both recipes (may need missing ingredient info for display) and cards
+      res.send(recipesAndCards);
+    }
   } catch (error) {
     res.status(500).send({ error: error.message });
-  }
-
-  
-  // // GET recipe matches and cards
-  // app.get('/recipes-with-cards', async (req, res) => {
-  
-  //         // Step 2: For each recipe, get its recipe card
-  //         const recipeCardsPromises = recipeMatches.map(async (recipe) => {
-  //             const recipeCardResponse = await axios.get(`https://api.spoonacular.com/recipes/${recipe.id}/card`, {
-  //                 params: {
-  //                     apiKey: SPOONACULAR_API_KEY
-  //                 }
-  //             });
-  //             return {
-  //                 ...recipe,  // Include the original recipe details
-  //                 card: recipeCardResponse.data.url  // Add the recipe card URL to the recipe object
-  //             };
-  //         });
-  
-  //         // Wait for all card requests to complete
-  //         const recipesWithCards = await Promise.all(recipeCardsPromises);
-  
-  //         // Step 3: Send the final results (recipes + their cards) to the client
-  //         res.json(recipesWithCards);
-  //     } catch (error) {
-  //         console.error('Error fetching recipes or cards:', error);
-  //         res.status(500).send('Error fetching recipe matches and cards');
-  //     }
-  // });
-  
-  
+  }  
 });
 
 module.exports = router;
