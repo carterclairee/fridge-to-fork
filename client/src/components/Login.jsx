dda763685e3684e1b5d393a2adf372c82c0a397f
Nannie Ingersdotter <emelie.l.gustafsson@gmail.com>
2024-10-20 13:36:34 +0200

    Login that kind of works

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, {useState}from "react";
+import axios from "axios";
 {/*import { use } from "../../../routes/users"; */}
 
 
@@ -85,7 +86,7 @@ function Register(){
    );     
 
    }
-
+/*
 function Login(){
 
     const [UserName, setUserName] = useState("");
@@ -112,7 +113,70 @@ function Login(){
         setErrorMessage(error.message);
     }
 };
+*/
 
+
+function Login() {
+    const [credentials, setCredentials] = useState({
+      UserName: "Test",
+      Password: "Test",
+    });
+  
+    const [data, setData] = useState(null);
+  
+    const { UserName, Password } = credentials;
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setCredentials({ ...credentials, [name]: value });
+    };
+  
+    const login = async () => {
+      // send a POST request to /api/auth/login with the username and password
+      try {
+        // axios return a data object with the response from the server
+        const { data } = await axios("/api/users/login", {
+          method: "POST",
+          data: credentials,
+        });
+  
+        console.log(data);
+        setData(data.message);
+  
+        //store it locally
+        localStorage.setItem("token", data.token);
+      } catch (error) {
+        console.log(error);
+        setData(error.message);
+      }
+    };
+
+    const logout = () => {
+        // remove the token from the local storage
+        localStorage.removeItem("token");
+        setData(null);
+      };
+    
+      // get the profile data
+      const requestData = async () => {
+        try {
+          const { data } = await axios("/api/users/profile", {
+            headers: {
+              authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          });
+    
+          console.log(data);
+          setData(data.UserName);
+        } catch (error) {
+          console.log(error);
+          setData(error.message);
+        }
+      };
+
+
+
+/*
 return (
     
     <form onSubmit={handleLoginSubmit}>
@@ -130,6 +194,51 @@ return (
     </form>
 
 
-);
+); */
+
+
+return (
+    <div>
+      <div>
+        <input
+          value={UserName}
+          onChange={handleChange}
+          name="UserName"
+          type="text"
+          className="form-control mb-2"
+        />
+        <input
+          value={Password}
+          onChange={handleChange}
+          name="Password"
+          type="password"
+          className="form-control mb-2"
+        />
+        <div className="d-flex gap-2 justify-content-center">
+          <button className="btn btn-primary" onClick={login}>
+            Log in
+          </button>
+          <button className="btn btn-outline-dark ml-2" onClick={logout}>
+            Log out
+          </button>
+        </div>
+      </div>
+      <div className="text-center p-4">
+        <button className=" btn btn-outline-primary" onClick={requestData}>
+          Request protected data
+        </button>
+      </div>
+
+      {data && (
+        <div className="text-center p-4">
+          <div className="alert">{data}</div>
+        </div>
+      )}
+    </div>
+  );
+
+
+
+
 }
 export { Register, Login }; 
