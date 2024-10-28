08095027713cb562778beb26a08f8ed350e0301e
Nannie Ingersdotter <emelie.l.gustafsson@gmail.com>
2024-10-28 17:30:58 +0100

    Home text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,13 +10,31 @@ export default function Home() {
 
 return (
     <div className="home-container">
-      <img src={veggies} alt="Home Background" className="home-image" />
-      <div className="text-field">
+        <div className="text-field">
+     
         <h1>Welcome to Fridge to Fork</h1>
 
-
         {/*isLoggedIn && <p className="greeting">Hello, welcome back!</p> */} 
+
+        <p className="app-description">
+          Fridge to Fork helps you make the most of what’s already in your fridge, 
+          connecting you with recipes that use up ingredients you have on hand. By matching 
+          your available ingredients with creative meal ideas, our app encourages resourcefulness, 
+          saves you money, and reduces food waste—all while making meal planning easier. Whether 
+          you’re looking to save on your grocery budget or make environmentally conscious choices, 
+          Fridge to Fork turns what you have into delicious, sustainable meals.
+        </p>
+
+
+
+        </div>
+         <div className="image-container">
+             <img src={veggies} alt="Home Background" className="home-image" />
       </div>
     </div>
   );
 }
+
+
+
+
